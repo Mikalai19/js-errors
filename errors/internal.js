87e4b5a6cf2416a5a5fbf8987ab5d5da5cb8f201
Mikalai19 <bertman29@gmail.com>
2021-11-17 15:21:53 -0800

    upd

--- errors/internal.js
+++ errors/internal.js
@@ -1,14 +1,14 @@
+function InternalError() {
+    function loop(x) {
+        if (x >= 10) // "x >= 10" is the exit condition
+            return;
+        // do stuff
+        loop(x + 1); // the recursive call
+    }
+    loop(0);
 
 
-function loop(x) {
-    if (x >= 10) // "x >= 10" is the exit condition
-        return;
-    // do stuff
-    loop(x + 1); // the recursive call
 }
-loop(0);
-
-
 
 
 
